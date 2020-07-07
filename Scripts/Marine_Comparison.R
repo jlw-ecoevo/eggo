23f9020ef600b907e27ccbe6deaab478fb68e4db
jake <jlweissman@gmail.com>
2020-07-07 10:36:36 -0700

    refseq and marine analysis

---
+++ Scripts/Marine_Comparison.R
@@ -0,0 +1,95 @@
+
+
+
+## JLW 2020 - EGGO Marine SAGs, MAGs, and Isolate Genomes
+
+# Load Packages ----------------------------------------------------------------
+
+
+library(dplyr)
+library(ggplot2)
+library(ggpubr)
+library(RColorBrewer)
+
+# Load Data --------------------------------------------------------------------
+
+setwd("~/eggo/Data/eggo-data")
+load("CodonStatistics_GORG.RData")
+load("CodonStatistics_MarRef.RData")
+isolate_df <- assembly_df
+load("CodonStatistics_Delmont.RData")
+mag_df_delmont <- mag_df
+load("CodonStatistics_Tully.RData")
+sag_df$CUBgenomic <- sag_df$CUBgenomic %>% lapply(median) %>% unlist()
+sag_df <- sag_df %>% mutate_all(unlist)
+sag_df <- sag_df %>% subset(nHE>=10)
+mag_df <- mag_df %>% subset(!is.na(d))
+mag_df <- mag_df %>% subset(nHE>=10)
+mag_df_delmont <- mag_df_delmont %>% subset(nHE>=10)
+
+# Plot -------------------------------------------------------------------------
+
+p1 <- ggplot(NULL,aes(x=d)) + 
+  geom_density(data=isolate_df,aes(fill="MarRef Genomes"),
+               alpha=0.5,color="white") + 
+  geom_density(data=mag_df,aes(fill="Tully et al. MAGs"),
+               alpha=0.5,color="white") + 
+  geom_density(data=mag_df_delmont,aes(fill="Delmont et al. MAGs"),
+               alpha=0.5,color="white") + 
+  geom_density(data=sag_df,aes(fill="GORG-tropics SAGs"),
+               alpha=0.5,color="white") + 
+  scale_x_log10(limit=c(1,50)) + theme_bw() +
+  scale_fill_brewer(palette = "Dark2") + 
+  theme(legend.title = element_blank(),
+        legend.position = "bottom",
+        legend.text = element_text(size=8),
+        legend.key.size = unit(0.8, "lines")) + 
+  geom_vline(xintercept = 5, lty = 2, color = "red") + 
+  xlab("Predicted Doubling Time (Hours)") 
+
+p1l <- ggplot(NULL,aes(x=d)) + 
+  geom_density(data=isolate_df,aes(fill="MarRef Genomes"),
+               alpha=0.5,color="white") + 
+  geom_density(data=mag_df,aes(fill="Tully et al. MAGs"),
+               alpha=0.5,color="white") + 
+  geom_density(data=mag_df_delmont,aes(fill="Delmont et al. MAGs"),
+               alpha=0.5,color="white") + 
+  geom_density(data=sag_df,aes(fill="GORG-tropics SAGs"),
+               alpha=0.5,color="white") + 
+  theme_bw() + xlim(0,30) +
+  scale_fill_brewer(palette = "Dark2") + 
+  theme(legend.title = element_blank(),legend.position = "bottom") + 
+  geom_vline(xintercept = 5, lty = 2, color = "red") + 
+  xlab("Predicted Doubling Time (Hours)") 
+
+x <- data.frame(Fast=rep(c(F,T),4),
+           Data=c("Isolate","Isolate",
+                  "MAG (Tully)","MAG (Tully)",
+                  "MAG (Delmont)","MAG (Delmont)",
+                  "SAG","SAG"),
+           Value=c(table(isolate_df$d<5)/nrow(isolate_df),
+                   table(mag_df$d<5)/nrow(mag_df),
+                   table(mag_df_delmont$d<5)/nrow(mag_df_delmont),
+                   table(sag_df$d<5)/nrow(sag_df)))
+p2 <- ggplot(x%>%subset(Fast==T),aes(x=Data,y=Value,fill=Data)) + 
+  geom_bar(position="stack", stat="identity",alpha=0.75) +
+  theme_bw() + xlab("") + 
+  ylab(expression("Proportion Doubling Times  <5 Hours")) +
+  # theme(axis.text.x = element_text(angle = 90,hjust=1),legend.position = "none") +
+  # scale_fill_brewer(palette = "Dark2",direction = -1)+
+  theme(legend.position = "none") +
+  scale_fill_manual(values = brewer.pal(4,"Dark2")[c(3,1,4,2)]) + 
+  ggpubr::rotate_x_text()
+
+setwd("~/eggo/Figs")
+pdf("MarineComparison.pdf",width=7,height=5)
+ggarrange(p1,p2,ncol=2,widths=c(5,1),
+          labels=c("(a)","(b)"),
+          hjust=0,
+          vjust=1)
+dev.off()
+
+setwd("~/eggo/Figs")
+pdf("MarineComparison_nolog.pdf",width=7,height=5)
+ggarrange(p1l,p2,ncol=2,widths=c(5,1))
+dev.off()
---
+++ Scripts/RefSeq_Analysis.R
@@ -0,0 +1,128 @@
+
+
+## JLW 2020 - EGGO RefSeq Analysis
+
+# Load Packages ----------------------------------------------------------------
+
+
+library(dplyr)
+library(ggplot2)
+library(mclust)
+library(ggpubr)
+library(RColorBrewer)
+
+set.seed("123456")
+
+# Load Data --------------------------------------------------------------------
+
+setwd("~/eggo/Data/eggo-data")
+load("CodonStatistics_Parks.RData")
+mag_df <- mag_df %>% subset(nHE>=10)
+load("CodonStatistics_RefSeq.RData")
+
+#Remove Genomes w/ Unrealistic #'s of Ribosomal Proteins
+quantile(assembly_df$nHE,c(0.005,0.995))
+assembly_df <- assembly_df %>% subset(nHE>50 & nHE<70)
+
+# Plot Overall Distribution ----------------------------------------------------
+
+setwd("~/eggo/Figs")
+pdf("RefSeq_Growth_All.pdf",width=7,height=5)
+ggplot(assembly_df,aes(x=d)) + geom_density() + 
+  scale_x_log10(limit=c(0.05,100)) + theme_bw() +
+  geom_vline(xintercept = 5, color = "red", lty = 2) + 
+  xlab("Predicted Doubling Time (Hours)")
+dev.off()
+
+# Subsample by genus -----------------------------------------------------------
+
+
+genus_df <- assembly_df %>% group_by(Genus) %>% sample_n(1)
+
+
+# Cluster growth rates ---------------------------------------------------------
+
+x <- log10(genus_df$d)
+m <- Mclust(x)
+
+
+# Plot Clusters ----------------------------------------------------------------
+
+p <- (colSums(m$z)/sum(m$z))
+cl_df <- data.frame(x=10^seq(-2,3,0.01),
+                    cl1=dnorm(seq(-2,3,0.01),
+                              mean=m$parameters$mean[1],
+                              sd=sqrt(m$p$variance$sigmasq))*p[1],
+                    cl2=dnorm(seq(-2,3,0.01),
+                              mean=m$parameters$mean[2],
+                              sd=sqrt(m$p$variance$sigmasq))*p[2])
+p1 <- ggplot() + geom_density(data=genus_df,aes(x=d),lwd=1.5) + 
+  scale_x_log10(limit=c(0.05,100)) +
+  geom_line(data=cl_df,aes(x=x,y=cl1,color="Cluster 1"),lwd=1) + 
+  geom_line(data=cl_df,aes(x=x,y=cl2,color="Cluster 2"),lwd=1) + 
+  theme_bw() + scale_color_brewer(palette = "Set1")  +
+  geom_vline(xintercept = 5, color = "red", lty = 2) + 
+  xlab("Predicted Doubling Time (Hours)")  + 
+  labs(color="")
+
+setwd("~/eggo/Figs")
+pdf("RefSeq_vs_Parks.pdf",width=10,height=5)
+ggplot() + 
+  geom_density(data=genus_df,aes(x=d,fill="RefSeq Assemblies"),alpha=0.5,color="white") + 
+  geom_density(data=mag_df,aes(x=d,fill="Parks et al. MAGs"),alpha=0.5,color="white") +
+  scale_x_log10(limit=c(0.05,100)) +
+  theme_bw() + scale_color_brewer(palette = "Set1")  +
+  geom_vline(xintercept = 5, color = "red", lty = 2) + 
+  xlab("Predicted Doubling Time (Hours)")  + 
+  labs(fill="")
+dev.off()
+
+
+# Plot by phylum ---------------------------------------------------------------
+
+table(genus_df$Phylum) %>% sort()
+pdf1_genus <- genus_df %>% subset(Phylum %in% c("Bacteroidetes",
+                                               "Actinobacteria",
+                                               "Firmicutes",
+                                               "Proteobacteria"))
+pdf2_genus <- genus_df %>% subset(Phylum %in% c("Chloroflexi",
+                                               "Planctomycetes",
+                                               "Cyanobacteria"))
+p2 <- ggplot(pdf1_genus,aes(x=d,fill=Phylum)) + 
+  geom_density(alpha=0.5) + scale_x_log10(limit=c(0.05,100))  +
+  geom_vline(xintercept = 5, color = "red", lty = 2) + 
+  theme_bw() + theme(legend.position = "bottom",
+                     legend.text = element_text(size=7),
+                     legend.key.size = unit(0.6, "lines")) + 
+  labs(fill="") + 
+  xlab("Predicted Doubling Time (Hours)") + 
+  scale_fill_manual(values = brewer.pal(7,"Set1")[1:4])
+
+p3 <- ggplot(pdf2_genus,aes(x=d,fill=Phylum)) + 
+  geom_density(alpha=0.5) + scale_x_log10(limit=c(0.05,100))  +
+  geom_vline(xintercept = 5, color = "red", lty = 2) + 
+  theme_bw() + theme(legend.position = "bottom",
+                     legend.text = element_text(size=7),
+                     legend.key.size = unit(0.6, "lines")) + 
+  labs(fill="") + 
+  xlab("Predicted Doubling Time (Hours)") + 
+  scale_fill_manual(values = brewer.pal(7,"Set1")[5:7])
+
+
+# Put it together --------------------------------------------------------------
+
+setwd("~/eggo/Figs")
+pdf("RefSeq_Growth.pdf",width=8,height=6)
+ggarrange(p1,
+          ggarrange(p2,
+                    p3,
+                    nrow=1,
+                    labels=c("(b)","(c)"),
+                    hjust=0,
+                    vjust=1),
+          nrow=2,
+          labels=c("(a)",""),
+          hjust=0,
+          vjust=1)
+dev.off()
+
